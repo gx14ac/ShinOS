49f60f9a02252f7c48d10a89c81904c37adaf22b
flekystyley <flekystyley@gmail.com>
2019-11-18 22:39:47 +0900

    fix space & world haha:(;

--- src/11_font_address/boot.asm
+++ src/11_font_address/boot.asm
@@ -1,111 +1,113 @@
 %include "../include/define.asm"
 %include "../include/macro.asm"
-    ORG BOOT_LOAD               ; Configuration Load Address.
+
+ORG BOOT_LOAD               ; Configuration Load Address.
 
 entry:
-    jmp ipl
-    times 90 - ($ - $$) db 0x90
+  jmp ipl
+  times 90 - ($ - $$) db 0x90
 
-    ;---------------------
-    ; - IPL(Initiali Loading Program).
-    ;---------------------
+ ;---------------------
+ ; - IPL(Initiali Loading Program).
+ ;---------------------
 ipl:
-    cli                         ; don't allow interrupt.
-    mov ax, 0x0000              ; AX = 0x0000;
+  cli                         ; don't allow interrupt.
+  mov ax, 0x0000              ; AX = 0x0000;
 
-    mov ds, ax                  ; ds = ax.
-    mov es, ax                  ; es = ax.
-    mov ss, ax                  ; ss = ax.
-    mov sp, BOOT_LOAD           ; sp = BOOT_:LOAD
+  mov ds, ax                  ; ds = ax.
+  mov es, ax                  ; es = ax.
+  mov ss, ax                  ; ss = ax.
+  mov sp, BOOT_LOAD           ; sp = BOOT_:LOAD
 
-    sti                         ; allow interupt.
+  sti                         ; allow interupt.
 
-    mov [BOOT + drive.no], dl   ; Saving Boot Drive.
+  mov [BOOT + drive.no], dl   ; Saving Boot Drive.
 
-    cdecl putc, .s0
+  cdecl putc, .s0
 
 
-    ;---------------------
-    ; - Import all remaining sectors.
-    ;---------------------
+  ;---------------------
+  ; - Import all remaining sectors.
+  ;---------------------
 
-    mov bx, BOOT_SECT - 1         ; Remaing Boot Sector.
-    mov cx, BOOT_LOAD + SECT_SIZE ; Next Loading Address.
+  mov bx, BOOT_SECT - 1         ; Remaing Boot Sector.
+  mov cx, BOOT_LOAD + SECT_SIZE ; Next Loading Address.
 
-    cdecl read_chs, BOOT, bx, cx  ; AX - read_chs(.chs, bx, cx)
+  cdecl read_chs, BOOT, bx, cx  ; AX - read_chs(.chs, bx, cx)
 
-    cmp ax, bx
+  cmp ax, bx
 
 .10Q: jz .10E
 .10T: cdecl putc, .e0
-    call reboot
+  call reboot
 .10E:
-    jmp stage_2
+  jmp stage_2
 
 .s0 db "Booting...", 0x0A, 0x0D, 0
 .e0 db "Error sector read", 0
 
 ALIGN 2, db 0
+
 BOOT:
-    istruc drive
-     at drive.no,   dw 0
-     at drive.cyln, dw 0
-     at drive.head, dw 0
-     at drive.sect, dw 2
-    iend
+  istruc drive
+  at drive.no,   dw 0
+  at drive.cyln, dw 0
+  at drive.head, dw 0
+  at drive.sect, dw 2
+  iend
 
-    ;---------------------
-    ; - Modules.
-    ;---------------------
-    %include "../modules/real_mode/putc.asm"
-    %include "../modules/real_mode/reboot.asm"
-    %include "../modules/real_mode/read_chs.asm"
+  ;---------------------
+  ; - Modules.
+  ;---------------------
+  %include "../modules/real_mode/putc.asm"
+  %include "../modules/real_mode/reboot.asm"
+  %include "../modules/real_mode/read_chs.asm"
 
-    ;---------------------
-    ; - Boot Flag.
-    ;---------------------
-    times 510 - ($ - $$) db 0x00
-    db 0x55, 0xAA
+  ;---------------------
+  ; - Boot Flag.
+  ;---------------------
+  times 510 - ($ - $$) db 0x00
+  db 0x55, 0xAA
 
 FONT:
 .seg: dw 0
 .off: dw 0
 
-    ;---------------------
-    ; - Modules.
-    ;---------------------
-    %include "../modules/real_mode/itoa.asm"
-    %include "../modules/real_mode/get_drive_param.asm"
-    %include "../modules/real_mode/get_fonts_addr.asm"
+ ;---------------------
+ ; - Modules.
+ ;---------------------
+ %include "../modules/real_mode/itoa.asm"
+ %include "../modules/real_mode/get_drive_param.asm"
+ %include "../modules/real_mode/get_fonts_addr.asm"
 
 stage_2:
-        ;---------------------
-        ; - Second Stage.
-        ;---------------------
-    cdecl putc, .s0
+ ;---------------------
+ ; - Second Stage.
+ ;---------------------
+ cdecl putc, .s0
 
-        ;---------------------
-        ; - Get drive information.
-        ;---------------------
-        cdecl get_drive_param, BOOT ; get_drive_param(DX, BOOT.CYLN).
-        cmp ax, 0
+ ;---------------------
+ ; - Get drive information.
+ ;---------------------
+ cdecl get_drive_param, BOOT ; get_drive_param(DX, BOOT.CYLN).
+ cmp ax, 0
 .10Q: jne .10E
 .10T: cdecl putc, .e0
         call reboot
 .10E:
-        ;---------------------
-        ; - Show drive information.
-        ;---------------------
-        mov   ax, [BOOT + drive.no]
-        cdecl itoa, ax, .p1, 2, 16, 0b0100
-        mov   ax, [BOOT + drive.cyln]
-        cdecl itoa, ax, .p2, 4, 16, 0b0100
-        mov   ax, [BOOT + drive.head]
-        cdecl itoa, ax, .p3, 2, 16, 0b0100
-        mov   ax, [BOOT + drive.sect]
-        cdecl itoa, ax, .p4, 2, 16, 0b0100
-        cdecl putc, .s1
-        jmp stage_3
+  ;---------------------
+  ; - Show drive information.
+  ;---------------------
+  mov   ax, [BOOT + drive.no]
+  cdecl itoa, ax, .p1, 2, 16, 0b0100
+  mov   ax, [BOOT + drive.cyln]
+  cdecl itoa, ax, .p2, 4, 16, 0b0100
+  mov   ax, [BOOT + drive.head]
+  cdecl itoa, ax, .p3, 2, 16, 0b0100
+  mov   ax, [BOOT + drive.sect]
+  cdecl itoa, ax, .p4, 2, 16, 0b0100
+  cdecl putc, .s1
+  jmp stage_3
 
 .s0: db "2nd stage...", 0x0A, 0x0D, 0
 
@@ -116,22 +118,16 @@ stage_2:
 .p4: db " ", 0x0A, 0x0D, 0
 
 .e0: db "Can't get drive parameter.", 0
-    ;---------------------
-    ; - Padding.
-    ; - This File is 8k bytes.
-    ;---------------------
 
 stage_3:
-    ; show fonts.
-    cdecl putc, .s0
+  cdecl putc, .s0
+  cdecl get_font_addr, FONT
 
-    cdecl get_font_addr, FONT
+  cdecl itoa, word[FONT.seg], .p1, 4, 16, 0b0100
+  cdecl itoa, word[FONT.off], .p2, 4, 16, 0b0100
+  cdecl putc, .s1
 
-    cdecl itoa, word[FONT.seg], .p1, 4, 16, 0b0100
-    cdecl itoa, word[FONT.off], .p2, 4, 16, 0b0100
-    cdecl putc, .s1
-
-    jmp $
+  jmp $
 
 .s0: db "3rd stage...", 0x0A, 0x0D, 0
 .s1: db "Font Address="
