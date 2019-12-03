c220bfc9a4266508843438299a19df3f2ce10015
flekystyley <flekystyley@gmail.com>
2019-12-03 20:55:54 +0900

    fix rtc;

--- src/modules/protect/rtc.asm
+++ src/modules/protect/rtc.asm
@@ -25,28 +25,31 @@ rtc_get_time:
     ; 0x09 | year
     ; **
     ;**************************
-    mov  al, 0x0A               ; al = 0x0A
+    mov  al, 0x0A               ; registerA
     out  0x70, al               ; output(0x70, AL)
-    in   al, 0x71               ; al
-    test al, 0x80
-    je   .10F
-    mov  eax, 1
-    jmp  .10E
-.10F:
-    mov al, 0x04
-    out 0x70, al
-    in  al, 0x71
-    shl eax, 8
+    in   al, 0x71               ; al = registerA
+    test al, 0x80               ; if(DM & UIP)
+    je   .10F                   ; {
+    mov  eax, 1                 ; ret = 1
+    jmp  .10E                   ; }
+.10F:                           ; else
 
-    mov al, 0x02
-    out 0x70, al
-    in  al, 0x71
-    shl eax, 8
+    ; RAM[0x04]:hour
+    mov al, 0x04                ; al = 0x04
+    out 0x70, al                ; output(0x70, al)
+    in  al, 0x71                ; al = inp(0x71)
+    shl eax, 8                  ; eax <<= 8
 
-    mov al, 0x00
-    out 0x70, al
-    in  al, 0x71
-    shl eax, 8
+    ; RAM[0x02]:minute
+    mov al, 0x02                ; al = 0x02
+    out 0x70, al                ; output(0x70, al)
+    in  al, 0x71                ; al = inp(0x71)
+    shl eax, 8                  ; eax <<= 8
+
+    ; RAM[0x00]:second
+    mov al, 0x00                ; al = 0x00
+    out 0x70, al                ; output(0x70, al)
+    in  al, 0x71                ; al = input(0x71)
 
     and eax, 0x00_FF_FF_FF      ; Only the lower 3 bytes are valid
     mov ebx, [ebp + 8]          ; dst = destination register
