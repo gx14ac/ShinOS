bc2017998cfdc119d2951163f04fc4e9739d3760
flekystyley <flekystyley@gmail.com>
2019-12-07 22:19:50 +0900

    add kernel.asm;

---
+++ src/30_int_timer/modules/int_timer.asm
@@ -0,0 +1,26 @@
+;************************************************
+; File to edit each time a task is added
+;************************************************
+
+int_timer:
+    pushad
+    push ds
+    push es
+
+    mov ax, 0x0010
+    mov ds, ax
+    mov es, ax
+
+    ; refresh interrupt count
+    inc dword[TIMER_COUNT]
+
+    ; clear the interrupt flag
+    outp 0x20, 0x20
+
+    pop es
+    pop ds
+    popad
+
+    iret
+ALIGN 4, db 0
+TIMER_COUNT: dq 0
