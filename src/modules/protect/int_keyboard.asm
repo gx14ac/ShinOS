2ec2430a7ea9b08205aac4e94a55fd9e2f26ac44
flekystyley <flekystyley@gmail.com>
2019-12-07 12:41:38 +0900

    add comment;

--- src/modules/protect/int_keyboard.asm
+++ src/modules/protect/int_keyboard.asm
@@ -9,19 +9,31 @@ int_keyboard:
     push  ds
     push  es
 
+    ;*****************************
+    ; segment settings for data
+    ;*****************************
     mov ax, 0x0010
     mov ds, ax
     mov es, ax
 
+    ;**********************
+    ; receive KBC Buffer
+    ;**********************
     in al, 0x60
 
+    ;*****************************
+    ; saving keycode
+    ;*****************************
     cdecl ring_wr, _KEY_BUFF, eax
 
+    ;**********************************
+    ; send interrupt terminate command
+    ;**********************************
     outp 0x20, 0x20
 
     pop es
     pop ds
 
-    iret
+    iret ; return interrupt process
 ALIGN 4, db 0
 _KEY_BUFF: times ring_buff_size db 0
