fc4e0e1a729a6463b0ad2ce99830509932d51383
flekystyley <flekystyley@gmail.com>
2019-12-13 15:27:23 +0900

    update build.sh

--- src/31_multi_task/build.sh
+++ src/31_multi_task/build.sh
@@ -3,6 +3,7 @@
 KERNEL_FILE="kernel.asm"
 
 if [ -e $KERNEL_FILE ]; then
+    rm -rf boot.bin boot.lst kernel.bin kernel.lst
     nasm boot.asm -o boot.bin -l boot.lst
     nasm kernel.asm -o kernel.bin -l kernel.lst
 
