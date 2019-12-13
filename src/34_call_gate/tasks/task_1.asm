e47b7cff3dba2eb5db8d806dbe9d9ff3783d5f3e
flekystyley <flekystyley@gmail.com>
2019-12-13 17:07:25 +0900

    update kernel.asm;

---
+++ src/34_call_gate/tasks/task_1.asm
@@ -0,0 +1,13 @@
+task_1:
+    ;*******************
+    ; display the char
+    ;*******************
+    cdecl draw_str, 63, 0, 0x07, .s0 ; draw_str()
+.10L:
+    mov   eax, [RTC_TIME]         ; get the clock
+    cdecl draw_time, 72, 0, 0x0700, eax ; display clock
+
+    jmp SS_TASK_0:0
+    jmp .10L
+
+.s0 db "Task-1", 0
