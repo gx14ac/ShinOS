91829595897f77b5ea054d03adfc90ad653c0c30
flekystyley <flekystyley@gmail.com>
2019-12-15 12:39:44 +0900

    add wait_tick;

---
+++ src/modules/protect/wait_tick.asm
@@ -0,0 +1,23 @@
+wait_tick:
+
+    push ebp
+    push ebp, esp
+
+    push eax
+    push ecx
+
+    mov ecx, [ebp + 8]          ; ecx = wait count
+    mov eax, [TIMER_COUNT]      ; eax = timer
+
+.10L: cmp [TIMER_COUNT], eax    ; while(TIMER != eax)
+    je    .10L                  ; (TIMER==eax)
+    inc   eax                   ; eax++
+    loop .10L                   ; loop
+
+    pop ecx
+    pop eax
+
+    mov esp, ebp
+    pop ebp
+
+    ret
