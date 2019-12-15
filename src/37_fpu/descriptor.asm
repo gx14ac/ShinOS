d1308bb68cd70af0cdb417ae7ec99ac7013384f5
flekystyley <flekystyley@gmail.com>
2019-12-15 12:40:31 +0900

    create new descriptor table;

--- src/37_fpu/descriptor.asm
+++ src/37_fpu/descriptor.asm
@@ -62,6 +62,38 @@ TSS_1:
 .ldt:    dd SS_LDT              ;* 96:ldt segment selecter
 .io:     dd 0                   ; 100:I/O MapBaseAddress
 
+;**************************************
+; TSS(Task State Segment) for TASK_1
+; Size 104 bytes
+;**************************************
+TSS_2:
+.link:   dd 0
+.esp0:   dd SP_TASK_2 - 512     ;*  4:esp0
+.ss0:    dd DS_KERNEL           ;*  8:
+.esp1:   dd 0                   ;* 12:esp1
+.ss1:    dd 0                   ;* 16:
+.esp2:   dd 0                   ;* 20:esp2
+.ss2:    dd 0                   ;* 24:
+.cr3:    dd 0                   ;* 28:CR3(PDBR)
+.eip:    dd task_2              ;  32:eip(start task1 address)
+.eflags: dd 0x0202              ;  36:EFLAGS(initial eflags value)
+.eax:    dd 0                   ;  40:eax
+.ecx:    dd 0                   ;  44:ecx
+.edx:    dd 0                   ;  48:edx
+.ebx:    dd 0                   ;  52:ebx
+.esp:    dd SP_TASK_2           ;  56:esp(initial stack pointer)
+.ebp:    dd 0                   ;  60:ebp
+.esi:    dd 0                   ;  64:esi
+.edi:    dd 0                   ;  68:edi
+.es:     dd DS_TASK_2           ;  72:es
+.cs:     dd CS_TASK_2           ;  76:cs(code segment selector for task1)
+.ss:     dd DS_TASK_2           ;  80:ss(data segment selector for task1)
+.ds:     dd DS_TASK_2           ;  84:ds(data segment selector for task1)
+.fs:     dd DS_TASK_2           ;  88:fs(data segment selector for task1)
+.gs:     dd DS_TASK_2           ;  92:gs(data segment selector for task1)
+.ldt:    dd SS_LDT              ;* 96:ldt segment selecter
+.io:     dd 0                   ; 100:I/O MapBaseAddress
+
 ;*****************************
 ; Global Descriptor Table
 ;*****************************
@@ -70,7 +102,8 @@ GDT:			dq  0x0000000000000000          ; NULL
 .ds_kernel:		dq	0x00CF92000000FFFF		; DATA 4G
 .ldt			dq	0x0000820000000000			; LDT descriptor
 .tss_0:             dq  0x0000890000000067      ; TSS descriptor
-.tss_1:             dq  0x0000890000000067      ; TSS descriptor
+.tss_1:             dq  0x0000890000000067      ; TSS descripto
+.tss_2:             dq  0x0000890000000067      ; TSS descriptor
 .call_gate:     dq  0x0000EC0400080000          ; 386 call gate (DPL = 3, count = 4, SEL = 8)
 .end:
 
@@ -82,6 +115,7 @@ DS_KERNEL equ .ds_kernel - GDT
 SS_LDT    equ .ldt       - GDT
 SS_TASK_0 equ .tss_0	 - GDT
 SS_TASK_1 equ .tss_1	 - GDT
+SS_TASK_2 equ .tss_2	 - GDT
 SS_GATE_0 equ .call_gate - GDT
 
 GDTR: dw GDT.end - GDT - 1
@@ -99,6 +133,8 @@ LDT: dq 0x0000000000000000      ; NULL
 ;*********************
 .cs_task_1: dq	0x00CFFA000000FFFF  ; CODE 4G
 .ds_task_1: dq	0x00CFF2000000FFFF  ; DATA 4G
+.cs_task_2: dq	0x00CFFA000000FFFF  ; CODE 4G
+.ds_task_2: dq	0x00CFF2000000FFFF  ; DATA 4G
 .end:
 
 ; `| 4` means that the segment granularity is 4k
@@ -106,5 +142,7 @@ CS_TASK_0 equ (.cs_task_0 - LDT) | 4     ; designated task0 cs segment selecter
 DS_TASK_0 equ (.ds_task_0 - LDT) | 4     ; designated task0 ds segment selecter
 CS_TASK_1 equ (.cs_task_1 - LDT) | 4 | 3 ; designated task1 cs segment selecter, privillege level 3
 DS_TASK_1 equ (.ds_task_1 - LDT) | 4 | 3 ; designated task1 ds segment selecter, privillege level 3
+CS_TASK_2 equ (.cs_task_2 - LDT) | 4 | 3 ; designated task2 cs segment selecter, privillege level 3
+DS_TASK_2 equ (.ds_task_2 - LDT) | 4 | 3 ; designated task2 ds segment selecter, privillege level 3
 
 LDT_LIMIT equ .end - LDT - 1
