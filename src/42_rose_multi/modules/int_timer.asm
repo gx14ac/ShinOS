b7e145746142c088fb7b3c5f79682801b964fa2b
flekystyley <flekystyley@gmail.com>
2019-12-17 23:27:26 +0900

    fix int_timer;

--- src/42_rose_multi/modules/int_timer.asm
+++ src/42_rose_multi/modules/int_timer.asm
@@ -27,6 +27,12 @@ int_timer:
     je .12L                     ; {
     cmp ax, SS_TASK_2           ; case (SS_TASK_2)
     je .13L                     ; {
+    cmp ax, SS_TASK_3
+    je  .14L
+    cmp ax, SS_TASK_4
+    je  .15L
+    cmp ax, SS_TASK_5
+    je  .16L
     jmp SS_TASK_0:0             ; // switching task0
     jmp .10E                    ; break
 .11L:
@@ -37,6 +43,15 @@ int_timer:
     jmp .10E
 .13L:
     jmp SS_TASK_3:0
+    jmp .10E
+.14L:
+    jmp SS_TASK_4:0
+    jmp .10E
+.15L:
+    jmp SS_TASK_5:0
+    jmp .10E
+.16L:
+    jmp SS_TASK_6:0
     jmp .10E
 .10E:
 
