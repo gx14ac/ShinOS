9e3955ff0344e9abc57bed224443eba4e8b2c80f
flekystyley <flekystyley@gmail.com>
2019-12-17 15:42:00 +0900

    create tss;

---
+++ src/42_rose_multi/modules/int_timer.asm
@@ -0,0 +1,49 @@
+;************************************************
+; File to edit each time a task is added
+;************************************************
+
+int_timer:
+    pusha
+    push ds
+    push es
+
+    mov ax, 0x0010
+    mov ds, ax
+    mov es, ax
+
+    ; refresh interrupt count
+    inc dword [TIMER_COUNT]
+
+    ; clear the interrupt flag
+    outp 0x20, 0x20
+
+    ;******************
+    ; switch the task
+    ;******************
+    str ax                      ; ax = tr // current task register
+    cmp ax, SS_TASK_0           ; case (SS_TASK_0)
+    je .11L                     ; { // (ax == SS_TASK_0)
+    cmp ax, SS_TASK_1           ; case (SS_TASK_1)
+    je .12L                     ; {
+    cmp ax, SS_TASK_2           ; case (SS_TASK_2)
+    je .13L                     ; {
+    jmp SS_TASK_0:0             ; // switching task0
+    jmp .10E                    ; break
+.11L:
+    jmp SS_TASK_1:0             ; // switching task1
+    jmp .10E                    ; break
+.12L:
+    jmp SS_TASK_2:0
+    jmp .10E
+.13L:
+    jmp SS_TASK_3:0
+    jmp .10E
+.10E:
+
+    pop es
+    pop ds
+    popa
+
+    iret
+ALIGN 4, db 0
+TIMER_COUNT: dd 0
---
+++ src/42_rose_multi/modules/paging.asm
@@ -0,0 +1,82 @@
+;************************************************
+; initialize paging
+;
+;---------------------------------------
+;          |____________|
+; 0010_5000|       (4K) | directory table
+;          =            =
+;          |____________|
+; 0010_6000|       (4K) | page table
+;          =            =
+;          |____________|
+; 0010_7000|       (4K) | page fault
+;          =            =
+;          |____________|
+;          |            |
+;
+;************************************************
+init_page:
+    pusha
+
+    ;******************************
+    ; create page transfer table
+    ;******************************
+    cdecl    page_set_4m, CR3_BASE
+
+    ;******************************
+    ; create page transfer table
+    ;******************************
+    mov	[0x0010_6000 + 0x107 * 4], dword 0
+
+    popa
+
+    ret
+
+;*********************************
+; create page directory function
+;*********************************
+page_set_4m:
+    ;***********************************************
+    ; ebp + 0 | ebp(base value)
+    ; ebp + 4 | eip(return value)
+    ; ebp + 8 | start page table memory position
+    ;***********************************************
+    push	ebp
+    mov	ebp, esp
+
+    pusha
+
+    ;****************************
+    ; create page directory(p=0)
+    ;****************************
+    cld
+    mov	edi, [ebp + 8]      ; edi = page directory head addr
+    mov	eax, 0x00000000     ; eax = 0
+    mov	ecx, 1024           ; ecx = 1024
+    rep     stosd               ; while (count--) *dst++
+
+    ;************************
+    ; select head entry
+    ;************************
+    mov	eax, edi            ; eax = edi(page directory head addr)
+    and	eax, ~0x0000_0FFF   ; eax &= ~0FFF // physics addr
+    or      eax, 7              ; eax |= 7 // allow Read & Write
+    mov	[edi - (1024 * 4)], eax ; select head entry
+
+    ;************************
+    ; configure page table
+    ;************************
+    mov	eax, 0x00000007     ; select physics address & allow RW(7)
+    mov	ecx, 1024           ; count = 1024
+
+.10L:
+    stosd                       ; *dst++
+    add	eax, 0x00001000     ; adr += 0x1000 // (4K = 4*1024 = 4096 = 0x1000)
+    loop	.10L                ; // while(--count(ecx register))
+
+    popa
+
+    mov	esp, ebp
+    pop	ebp
+
+    ret
