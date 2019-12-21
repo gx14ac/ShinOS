d682388181d5fc68fbb94022591416b0a61c04d0
flekystyley <flekystyley@gmail.com>
2019-12-21 14:34:28 +0900

    fix memcmp;

--- src/modules/real_mode/memcmp.asm
+++ src/modules/real_mode/memcmp.asm
@@ -1,33 +1,33 @@
 memcmp:
-    push bp
-    mov bp, sp
+    push	bp
+    mov	bp, sp
 
-    push bx
-    push cx
-    push dx
-    push si
-    push di
+    push	bx
+    push	cx
+    push	dx
+    push	si
+    push	di
 
     cld
-    mov si, [bp+4]
-    mov di, [bp+6]
-    mov cx, [bp+8]
+    mov	si, [bp + 4]
+    mov	di, [bp + 6]
+    mov	cx, [bp + 8]
 
-    repe cmpsb ; si == di
-    jnz .10F   ; false jmp to .10F
-    mov ax, 0  ; return true
-    jmp .10E   ; end
-.10F
-    mov ax, -1 ; return false
-.10E
+    repe    cmpsb ; si == di
+    jnz	.10F   ; false jmp to .10F
+    mov	ax, 0  ; return true
+    jmp	.10E   ; end
+.10F:
+    mov	ax, -1 ; return false
+.10E:
 
-    pop di
-    pop si
-    pop dx
-    pop cx
-    pop bx
+    pop	di
+    pop	si
+    pop	dx
+    pop	cx
+    pop	bx
 
-    mov sp, bp
-    pop bp
+    mov	sp, bp
+    pop	bp
 
     ret
