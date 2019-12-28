c3d8417cac50a5a5b8522d5a671b7e138406ed39
flekystyley <flekystyley@gmail.com>
2019-12-28 14:21:43 +0900

    update bootpack;

--- playground/src/bootpack.c
+++ playground/src/bootpack.c
@@ -52,6 +52,24 @@ struct BootInfo {
     char *vram;                       // 4 byte
 };
 
+struct SegmentDescriptor { // 8byte
+    short limit_low, base_low; // 4byte
+    char base_mid, access_right; // 2byte
+    char limit_high, base_high; // 2byte
+};
+
+struct GateDescriptor { // 10byte
+    short offset_low, selector; // 4byte
+    char dw_count, access_right; // 2byte
+    short offset_high; // 4byte
+};
+
+void set_segmdesc(struct SegmentDescriptor *sd, unsigned int limit, int base, int ar);
+void set_gatedesc(struct GateDescriptor *gd, int offset, int selector, int ar);
+void init_gdtidt(void);
+void load_gdtr(int limit, int addr);
+void load_idtr(int limit, int addr);
+
 void HariMain(void)
 {
     struct BootInfo *binfo = (struct BootInfo *) 0x0ff0;
@@ -251,6 +269,68 @@ void putblock8_8(char *vram, // 0xa0000
             vram[(py0+y) * vxsize + (px0+x)] = buf[y * bxsize + x];
         }
     }
+
+    return;
+}
+
+/*
+  - initialize global & interrupt descriptor table
+*/
+void init_gdtidt(void)
+{
+    // GDT(Gloabal(Segment) Descriptor Table) : 0x00270000 ~ 0x0027ffff (65535 byte)
+    struct SegmentDescriptor *gdt = (struct SegmentDescriptor *) 0x00270000; // 0x00270000 ~ 0x00270009 (8 byte)
+    // GateDescriptor : 0x0026f800 ~ 0x0026ffff (28672 byte)
+    struct GateDescriptor *idt = (struct GateDescriptor *) 0x0026f800;
+
+    // initialize GDT
+    for (int i = 0; i < 8192; i++) {
+        set_segmdesc(gdt + i, 0, 0, 0);
+    }
+    set_segmdesc(gdt + 1, 0xffffffff, 0x00000000, 0x4092);
+    set_segmdesc(gdt + 2, 0x0007ffff, 0x409a);
+    load_gdtr(0xffff, 0x00270000);
+
+    for (int i = 0; i < 256; i++ ) {
+        set_gatedesc(idt + i, 0, 0, 0);
+    }
+    load_idtr(0x7ff, 0x0026f800);
+
+    return;
+}
+
+void set_segmdesc(struct SegmentDescriptor *sd,
+                  unsigned int limit,
+                  int base,
+                  int ar
+    )
+{
+    if (limit > 0xffff) {
+        ar |= 0x8000; // G_bit = 1
+        limit /= 0x1000;
+    }
+
+    sd->limit_low = limit & 0xffff;
+    sd->base_low = base & 0xffff;
+    sd->base_mid = (base >> 16) & 0xff;
+    sd->access_right = ar & 0xff;
+    sd->limit_high = ((limit >> 16) & 0x0f) | ((ar >> 8) & 0xf0);
+    sd->base_high = ((base >> 24)) & 0xff;
+
+    return;
+}
+
+void set_gatedesc(struct GateDescriptor *gd,
+                  int offset,
+                  int selector,
+                  int ar
+    )
+{
+    gd->offset_low = offset & 0xffff;
+    gd->selector = selector;
+    gd->dw_count = ((ar >> 8)) & 0xff;
+    gd->access_right = ar & 0xff;
+    gd->offset_high = (offset >> 16) & 0xffff;
 
     return;
 }
