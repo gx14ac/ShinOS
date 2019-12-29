762f1ac6aec98232ac54c92c6a57920134bb35d1
flekystyley <flekystyley@gmail.com>
2019-12-30 02:35:38 +0900

    update bootpack.c;

--- playground/src/bootpack.c
+++ playground/src/bootpack.c
@@ -1,59 +1,4 @@
-#define COL8_000000     0
-#define COL8_FF0000     1
-#define COL8_00FF00     2
-#define COL8_FFFF00     3
-#define COL8_0000FF     4
-#define COL8_FF00FF     5
-#define COL8_00FFFF     6
-#define COL8_FFFFFF     7
-#define COL8_C6C6C6     8
-#define COL8_840000     9
-#define COL8_008400     10
-#define COL8_848400     11
-#define COL8_000084     12
-#define COL8_840084     13
-#define COL8_008484     14
-#define COL8_848484     15
-
-extern void io_hlt(void);
-extern void io_cli(void);
-extern void io_out8(int port, int data);
-extern int  io_load_eflags(void);
-extern void io_store_eflags(int eflags);
-
-extern void init_palette(void);
-extern void set_palette(int start, int end, unsigned char *rgb);
-extern void boxfill_8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
-extern void init_screen(char *vram, int x, int y);
-extern void put_font8(char *vram, int xsize, int x, int y, char c, char *font);
-extern void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
-extern void sprintf(char *str, char *fmt, ...);
-extern void init_mouse_cursor8(char *mouse, char background_color);
-extern void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize);
-
-extern void set_segmdesc(struct SegmentDescriptor *sd, unsigned int limit, int base, int ar);
-extern void set_gatedesc(struct GateDescriptor *gd, int offset, int selector, int ar);
-extern void init_gdtidt(void);
-extern void load_gdtr(int limit, int addr);
-extern void load_idtr(int limit, int addr);
-
-struct BootInfo {
-    char cyls, leds, vmode, reserve;  // 1 byte
-    short scrnx, scrny;               // 2 byte
-    char *vram;                       // 4 byte
-};
-
-struct SegmentDescriptor { // 8byte
-    short limit_low, base_low; // 4byte
-    char base_mid, access_right; // 2byte
-    char limit_high, base_high; // 2byte
-};
-
-struct GateDescriptor { // 10byte
-    short offset_low, selector; // 4byte
-    char dw_count, access_right; // 2byte
-    short offset_high; // 4byte
-};
+#include "bootpack.h"
 
 void HariMain(void)
 {
@@ -77,6 +22,10 @@ void HariMain(void)
 
     sprintf(s, "scrnx = %d", binfo->scrnx);
     putfonts8_asc(binfo->vram, binfo->scrnx, 16, 64, COL8_FFFFFF, s);
+
+    io_out8(PIC0_IMR, 0xf9);
+    io_out8(PIC1_IMR, 0xef);
+
     for (;;) {
         io_hlt();
     }
