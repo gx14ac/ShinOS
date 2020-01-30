ed8b216e93261f006458533998645d38725ddd29
flekystyley <flekystyley@gmail.com>
2020-01-30 16:51:54 +0900

    update dsctbl.c;

--- playground/src/dsctbl.c
+++ playground/src/dsctbl.c
@@ -8,12 +8,12 @@
 void init_gdtidt(void)
 {
     // GDT(Gloabal(Segment) Descriptor Table) : 0x00270000 ~ 0x0027ffff (65535 byte)
-    struct SegmentDescriptor *gdt = (struct SegmentDescriptor *) 0x00270000; // 0x00270000 ~ 0x00270009 (8 byte)
+    struct SegmentDescriptor *gdt = (struct SegmentDescriptor *) ADR_GDT; // 0x00270000 ~ 0x00270009 (8 byte)
     // GateDescriptor : 0x0026f800 ~ 0x0026ffff (28672 byte)
-    struct GateDescriptor *idt = (struct GateDescriptor *) 0x0026f800;
+    struct GateDescriptor *idt = (struct GateDescriptor *) ADR_IDT;
 
     // initialize GDT
-    for (int i = 0; i < 8192; i++) {
+    for (int i = 0; i < LIMIT_GDT; i++) {
         set_segmdesc(gdt + i, 0, 0, 0);
     }
     // CPU All Segment(4GB)
@@ -23,14 +23,15 @@ void init_gdtidt(void)
     load_gdtr(LIMIT_GDT, ADR_GDT);
 
     // initialize IDT
-    for (int i = 0; i < 256; i++) {
+    for (int i = 0; i < LIMIT_IDT; i++) {
         set_gatedesc(idt + i, 0, 0, 0);
     }
-    load_idtr(0x7ff, 0x0026f800);
+    load_idtr(LIMIT_IDT, ADR_IDT);
 
     /* configure IDT */
-    set_gatedesc(idt + 0x21, (int)asm_inthandler21, 2 * 8, AR_INTGATE32);
-    set_gatedesc(idt + 0x2c, (int)asm_inthandler2c, 2 * 8, AR_INTGATE32);
+    set_gatedesc(idt + 0x21, (int) asm_inthandler21, 2 * 8, AR_INTGATE32);
+	set_gatedesc(idt + 0x27, (int) asm_inthandler27, 2 * 8, AR_INTGATE32);
+	set_gatedesc(idt + 0x2c, (int) asm_inthandler2c, 2 * 8, AR_INTGATE32);
 
     return;
 }
@@ -42,17 +43,17 @@ void set_segmdesc(struct SegmentDescriptor *sd,
                   int ar
     )
 {
-    if (limit > 0xffff) {
+    if (limit > 0xfffff) {
         ar |= 0x8000; // OR
         limit /= 0x1000;
     }
 
-    sd->limit_low = limit & 0xffff;
-    sd->base_low = base & 0xffff;
-    sd->base_mid = (base >> 16) & 0xff; // right shiift
+    sd->limit_low    = limit & 0xffff;
+    sd->base_low     = base & 0xffff;
+    sd->base_mid     = (base >> 16) & 0xff; // right shiift
     sd->access_right = ar & 0xff;
-    sd->limit_high = ((limit >> 16) & 0x0f) | ((ar >> 8) & 0xf0);
-    sd->base_high = ((base >> 24)) & 0xff;
+    sd->limit_high   = ((limit >> 16) & 0x0f) | ((ar >> 8) & 0xf0);
+    sd->base_high    = ((base >> 24)) & 0xff;
 
     return;
 }
@@ -63,11 +64,11 @@ void set_gatedesc(struct GateDescriptor *gd,
                   int ar
     )
 {
-    gd->offset_low = offset & 0xffff;
-    gd->selector = selector;
-    gd->dw_count = ((ar >> 8)) & 0xff;
+    gd->offset_low   = offset & 0xffff;
+    gd->selector     = selector;
+    gd->dw_count     = ((ar >> 8)) & 0xff;
     gd->access_right = ar & 0xff;
-    gd->offset_high = (offset >> 16) & 0xffff;
+    gd->offset_high  = (offset >> 16) & 0xffff;
 
     return;
 }
